d16042cab91ee4c61e9b6077ed0270231e8cbeb9
onlyphantom <samuel@outlook.sg>
2019-03-20 10:40:16 +0800

    interim progress update

--- downloader/multiconn/multiconn-server.py
+++ downloader/multiconn/multiconn-server.py
@@ -1,12 +1,14 @@
 # =====================================================
 #          Purpose [TODO: REPLACE THIS]:
-# Illustrate the Socket > Bind > Listen > Accept 
-# > Receive > Send > Close paradigm illustrated in
-# 2_tcpipmodel.md
-
-
-# select() allows server to MONITOR multiple client-connected
-# connections to see which client has sent the data to process
+# This version of server uses .setblocking(False) to 
+# prevent server from blocking. 
+# Same loop: Socket > Bind > Listen > Accept 
+# > Receive > Send > Close paradigm 
+#
+# Listening socket: 
+# s.socket().bind().listen().setblocking(F) -> register with Sel
+# Accept connections from listening socket:
+# s.accept().setblocking(F) -> register with Sel
 # 
 # socket.listen() # ready to accept connections
 # conn.recv
@@ -16,6 +18,7 @@
 import selectors
 import socket
 import sys
+import types
 
 HOST, PORT = sys.argv[1], int(sys.argv[2])
 if len(sys.argv) != 3:
@@ -24,8 +27,8 @@ if len(sys.argv) != 3:
 
 sel = selectors.DefaultSelector()
 
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lsock.bind((HOST, PORT))
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+lsock.bind((HOST, PORT)) # lsock = listening socket
 lsock.listen()
 print('listening on', (HOST, PORT))
 
@@ -40,10 +43,10 @@ lsock.setblocking(False)
 sel.register(lsock, selectors.EVENT_READ, data=None)
 while True:
     # blocks until there are sockets ready for I/O
-    # when unblock, return a list of (key,event) tuples one for
-    # each socket. key is a SelectorKey that contains the 
-    # socket object (.fileobj). mask is an event mask of the
-    # operations that are ready. 
+    # when unblock, return a list of (key, event) tuples 
+    # one for each socket. key is a SelectorKey that contains 
+    # the socket object (.fileobj). mask is an event mask of 
+    # the operations that are ready. 
     # 
     # When key.data is None, it's from the listening socket (server).
     # We accept() the connection by registering the new socket 
@@ -58,8 +61,28 @@ while True:
             service_connection(key, mask)
 
 def accept_wrapper(sock):
-    pass
+    # listening socket was registered for selectors.EVENT_READ so it is
+    # ready to read. Call sock.accept() and put socket in non-blocking
+    conn, addr = sock.accept()
+    print("accepted connection from", addr)
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    # register again, but this socket READ / WRITE and return data
+    sel.register(conn, events, data=data)
 
 def service_connection(key, mask):
-    pass
-
+    # key is tuple returned from select() containing socket object (fileobj)
+    # and data object. mask contains events that are ready 
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print('closing connection to', data.addr)
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        pass
